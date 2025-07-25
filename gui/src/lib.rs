e2ebed14540202ab349d64f9e4ffc6f25be0abef
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-07-25 20:40:23 +0300

    start gui dev

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -2,8 +2,19 @@ use iced::Center;
 use iced::widget::{Column, button, column, text};
 
 #[derive(Default)]
-pub struct Counter {
-    value: i64,
+pub struct Messaging {
+    income: String,
+    message: String,
+}
+
+pub struct Register {
+    selection: i32,
+    username: String,
+}
+
+pub struct Join {
+    token: String,
+    selection: i32,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -12,22 +23,15 @@ pub enum Message {
     Decrement,
 }
 
-impl Counter {
+impl Messaging {
     pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
+        todo!()
     }
 
     pub fn view(&self) -> Column<'_, Message> {
         column![
             button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
+            // text(self.value).size(50),
             button("Decrement").on_press(Message::Decrement)
         ]
             .padding(20)
