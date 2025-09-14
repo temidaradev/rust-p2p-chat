508ac2e21cb9d1958769a3b1f7378aa910a98e48
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-09-14 20:17:56 +0300

    will migrate to dioxus

--- p2p-chat/src/gui/handler.rs
+++ p2p-chat/src/gui/handler.rs
@@ -6,9 +6,21 @@ pub struct Handler {}
 
 impl Handler {
     pub fn handle_gui() -> Result<(), Box<dyn Error>> {
-        let ui = AppWindow2::new()?;
+        let main = StartWindow::new()?;
+        let main_handle = main.as_weak();
+        let join = JoinWindow::new()?;
+        let join_handle = join.as_weak();
 
-        ui.run()?;
+        main.show();
+
+        main.on_switch_to_join_window(move || {
+            let main = main_handle.unwrap();
+            let join = join_handle.unwrap();
+            join.show();
+            main.hide();
+        });
+
+        main.run()?;
 
         Ok(())
     }
